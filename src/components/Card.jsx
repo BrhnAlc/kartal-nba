5258f234071297cd5165e12910ddbf31b20683f3
burhan <alacaburhan122016@gmail.com>
2023-07-13 15:17:54 +0300

    containerr

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,9 @@
 
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 
@@ -8,7 +12,13 @@ const Card = () => {
     <div >
 
 <Form.Control type="search" placeholder="Oyuncu Ara ..." />
-
+<Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+   
+    </Container>
 
     </div >
   )
