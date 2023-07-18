01391a76dd1dcc64b57a70aa6393d4d9d3093858
burhan <alacaburhan122016@gmail.com>
2023-07-18 14:30:43 +0300

    playearCard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import Row from "react-bootstrap/Row";
 // import Col from "react-bootstrap/Col";
 import {data} from "../helper/data";
 import PlayerCard from './PlayerCard';
-import { Col } from 'react-bootstrap';
+
 
 const Card = () => {
     const [ara,setAra] = useState("")
@@ -13,7 +13,7 @@ const Card = () => {
     const handleChange = (e) =>{
         console.log(e.target.value)
         setAra(e.target.value) //! setter mtodları asenkron olarak çalışır.
-        // console.log(search)
+        // console.log(ara);
         
     }
     console.log(ara);
@@ -30,7 +30,7 @@ const Card = () => {
         onChange={handleChange}
       />
       <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={1} sm={2} md={3} lg={4} className="justify-content-center g-3">
+        <Row xs={1} sm={2} md={3} lg={4} className="justify-content-center ">
           
           {filteredData.map((item, i) => (
             <PlayerCard key={i} {...item} />
@@ -43,55 +43,4 @@ const Card = () => {
 
 export default Card;
 
-
-
-
-// import Form from 'react-bootstrap/Form';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import { useState } from 'react';
-// import {data} from  "../helper/data";
-// import PlayerCard from './PlayerCard';
-
-
-
-
-
-// const Card = () => {
-// const [ara , setAra]= useState("")
-// console.log(data);
-// const handleChange=(e)=>{
-//   console.log(e.target.value);
-
-//   setAra(e.target.value)
-//   // console.log(ara);
-
-//   const filteredData=data.filter(item=>item.name.toLowerCase().includes(ara.trim().toLowerCase()))
-
-//   console.log("filter",filteredData);
-
-// }
-
-//   return (
-//     < >
-
-// <Form.Control type="search" placeholder="Oyuncu Ara ..." onChange={handleChange}  />
-// <Container className='p-3 rounded-4 card-container my-3'>
-//       <Row xs={2} md={4} lg={6} className='justify-content-center g-3 my-3 '>
-//         <Col>1 of 2</Col>
-
-//         {filteredData.map((player, i) => (
-//             <PlayerCard key={i} {...player} />
-//           ))}
-
-//       </Row>
-   
-//     </Container>
-
-//     </>
-//   )
-// }
-
-// export default Card
 
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,25 +1,40 @@
 
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
-
-
-
-const PlayerCard = ({name, img,statistics}) => {
-  return (<Col>
-  <Card>
-  <Card.Img variant="top" src={img} height="350px"/>
-  <ul>
-    {
-statistics.map((item,i)=><li className="list-unstyled h5 text-start" key={i}> 🏀🏀{item}</li>)
-
-    }
-  </ul>
-  <Card.Footer>
-  <Card.Title>{name}</Card.Title>
-  </Card.Footer>
-</Card>
-</Col>);
-}
+const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
+  
+  return (
+    <Col>
+      <Card onClick={()=>setShowImage(!showImage)} className="player-card" role="button">
+        {showImage ? (
+          <Card.Img
+            variant="top"
+            src={img}
+            height="350px"
+            
+          />
+        ) : (
+          <ul 
+          className="m-auto"
+      
+          >
+            {statistics.map((item, i) => (
+              <li className="list-unstyled h5 text-start" key={i}>
+                {" "}
+                🏀 {item}
+              </li>
+            ))}
+          </ul>
+        )}
+        <Card.Footer>
+          <Card.Title>{name}</Card.Title>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+};
 
 export default PlayerCard;
